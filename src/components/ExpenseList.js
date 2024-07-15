5a7d1fde8f30de72361cfa90d6301cda48761e69
jeong won <jw221356@naver.com>
2024-07-15 18:55:58 +0900

    expense-ltem 기본적인 부분 완성!

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import "./ExpenseList.css"
+import Expenseltem from './Expenseltem'
 
 export class ExpenseList extends Component {
   render() {
     return (
       <React.Fragment>
         <ul className='list'>
-            {/* ExpenseItem */}
+            <Expenseltem />
         </ul>
         <button className='btn'>
             목록 지우기
--- src/components/Expenseltem.js
+++ src/components/Expenseltem.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import "./ExpenseItem.css"
 
 export class Expenseltem extends Component {
   render() {
     return (
-      <div>Expenseltem</div>
+      <li className='item'>
+        <div className='info'>
+            <span className='expense'>헬스 등록</span>
+            <span className='amount'> 1000 원</span>
+        </div>
+            <div>
+                <button className='edit-btn'>수정</button>
+                <button className='clear-btn'>삭제</button>
+            </div>
+      </li>
     )
   }
 }
