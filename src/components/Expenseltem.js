ef343843748c010fe41c4db07402cd684a15fbef
jeong won <jw221356@naver.com>
2024-07-15 17:28:23 +0900

    컴포넌트 파일 추가

---
+++ src/components/Expenseltem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Expenseltem extends Component {
+  render() {
+    return (
+      <div>Expenseltem</div>
+    )
+  }
+}
+
+export default Expenseltem
