056bcb32363c0a0b0dca518ae6227c3d3328b128
jeong won <jw221356@naver.com>
2024-07-15 17:04:17 +0900

    UI 틀 생성

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Component } from "react";
+
+
+
+class App extends Component {
+  render() {
+   return (
+    <main className="main-container">
+      <h1>예산 계산기</h1>
+
+      <div style={{width:'100%', backgroundColor:'white', padding:'1rem'}}>
+        {/* Expense Form */}
+      </div>
+      <div style={{width:'100%', backgroundColor:'white', padding:'1rem'}}>
+        {/* Expense List */}
+      </div>
+      <div style={{display:'flex', justifyContent:'end', marginTop:'1rem'}}>
+        <p style={{fontSize:'2rem'}}>
+          총지출: 
+          <span>원</span>
+        </p>
+      </div>
+    </main>
+   )
+  }
+}
+
+export default App;
