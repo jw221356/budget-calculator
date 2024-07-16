8498b2712a3b7d22e02c6af66bf8d245ec7450c1
jeong won <jw221356@naver.com>
2024-07-16 17:49:48 +0900

    Hooks 사용하여 함수형 컴포넌트로 변경

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import "./ExpenseList.css"
 import Expenseltem from './Expenseltem'
 import { MdDelete } from 'react-icons/md'
 
-export class ExpenseList extends Component {
-  
-  render() {
-    console.log(this.props.initialExpenses)
-    return (
-      <React.Fragment>
-        <ul className='list'>
-            {this.props.initialExpenses.map(expense => {
-              return (
-                <Expenseltem 
-                  expense = {expense} 
-                  key = {expense.id}
-                  handleDelete = {this.props.handleDelete}
-                />
-              )
-            })}
-        </ul>
-        <button className='btn'>
-            목록 지우기
-            <MdDelete className='btn-icon'/>
-        </button>
-      </React.Fragment>
-    )
-  }
+const ExpenseList = ({handleDelete, initialExpenses}) => {
+  return (
+    <React.Fragment>
+      <ul className='list'>
+          {initialExpenses.map(expense => {
+            return (
+              <Expenseltem 
+                expense = {expense} 
+                key = {expense.id}
+                handleDelete = {handleDelete}
+              />
+            )
+          })}
+      </ul>
+      <button className='btn'>
+          목록 지우기
+          <MdDelete className='btn-icon'/>
+      </button>
+    </React.Fragment>
+  )
 }
 
 export default ExpenseList
